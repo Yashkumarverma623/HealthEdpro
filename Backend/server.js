8d28ff4a34ebb39cad413fb135d4198d093af8a6
Yash <yashkumarverma623@gmail.com>
2025-03-26 19:33:59 +0530

    bbvkhd

--- Backend/server.js
+++ Backend/server.js
@@ -19,6 +19,16 @@ const corsOptions = {
 };
 
 
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
