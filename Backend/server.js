10019eac026ae7f523f53ad0498e2cd09338e3b7
Yash <yashkumarverma623@gmail.com>
2025-03-26 18:50:14 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -10,13 +10,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const corsOptions = {
-  origin: process.env.CORS_ORIGIN || '*', 
+app.use(cors({
+  origin: 'https://health-edpro-repo.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
-};
+}));
 
-app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
