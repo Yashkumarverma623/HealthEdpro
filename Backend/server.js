565b88d9bdddaa1bddae7f0eb6a5977de1b923d7
Yash <yashkumarverma623@gmail.com>
2025-03-26 17:51:33 +0530

    initial commit

---
+++ Backend/server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connect = require("./connect");
+const posts = require("./postRoutes");
+const users = require("./userRoutes");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN || '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/posts', posts);
+app.use('/api/users', users);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM signal received: closing HTTP server');
+  app.close(() => {
+    console.log('HTTP server closed');
+  });
+});
+
+const startServer = async () => {
+  try {
+    await connect.connectToServer();
+    app.listen(PORT, () => {
+      console.log(`Server is running at port: ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Failed to start server:', error);
+    process.exit(1);
+  }
+};
+
+startServer();
